0ade2d49711d33d5630ca3316691062058fb4ac9
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-23 19:40:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,90 +1,43 @@
 <?php
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
+  error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+echo "<h1>WELCOME TO STARTUPWALA</h1> \n";
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+require_once("heroku_postgres_database.php");
 
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  
- return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-echo $pg_conn;
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-}
- else 
-{
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-
- print "\n";
-
- //$sql = "CREATE TABLE data (Name VARCHAR(20))";
-
-  //$result = pg_query($pg_conn, $sql);
-
- // $sql2 = "INSERT INTO data (Name) values ('Startupwala')";
-//
-  //$result2 = pg_query($pg_conn, $sql2);
- print "\n";
-  //echo $result2;
-
-   $sql3 = "SELECT * FROM data";
-
-   $result3 = pg_query($pg_conn, $sql3);
-
-  
-   while( $single_row = pg_fetch_array($result3))
-    {
-
-   	 print "\n";
-
-   	 var_dump($single_row);
-
-
-   }
-
-   	$fetch_from_saleforce_contact=  "SELECT  firstname, lastname, email, phone FROM salesforce.contact
-						ORDER BY
-						  systemmodstamp DESC";
-
-						  $fetched_saleforce_data = pg_query($pg_conn, $fetch_from_saleforce_contact);
-
-						  
-
-						  while( $row = pg_fetch_array($fetched_saleforce_data))
-						   {
-
-						   	 print "\n";
-
-						   	 var_dump($row);
-
-
-						   }
+$herokupostgrsdatabse = new HerokuPostgresDatabase();
 
 
 
-// query($sql);
+$sql = "CREATE TABLE demobmc(
+   user_id SERIAL PRIMARY KEY,
+   username VARCHAR
+   )";
 
-//  public function query($sql)
-//     {
-//     	global $pg_conn;
-//     	global $sql;
-//      $result = pg_query($pg_conn, $sql);
 
-//      echo $result;
-//         // $this->confirm_query($result);
-//         return $result;
-//     }
+
+// $sql2 = "INSERT INTO startupwala_user (username,password) VALUES ('Avinash','123')";
+
+
+// $sql = "CREATE TABLE data (Name VARCHAR(20))";
+
+  $result = $herokupostgrsdatabse->query($sql);
+
+
+ // $result2 = $herokupostgrsdatabse->query($sql2);
+
+//$sql3 = "select username from startupwala_user";
+  //$result3 = $herokupostgrsdatabse->query($sql3);
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "\n <h3>following users are already registered:<h3>";
+  //while( $row = pg_fetch_array($result3))
+              // {
+
+             //  }
+
 
 ?>
