e486f132dbeeb794b2572cea551428ab5af21e49
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-08-24 18:58:14 +0530

    Update index3.php

--- index3.php
+++ index3.php
@@ -1,30 +1,26 @@
 <?php
 require_once("heroku_postgres_database.php");
 
-?>
-<pre>
-<?php
-
 
 $herokupostgrsdatabse = new HerokuPostgresDatabase();
 
 
 		//create new table query
 
-	// $create_table_query = "CREATE TABLE registered_users(
- //   user_id SERIAL PRIMARY KEY,
- //   first_name VARCHAR(50),
- //   last_name VARCHAR(50),
+	$create_table_query = "CREATE TABLE registered_users(
+   user_id SERIAL PRIMARY KEY,
+   first_name VARCHAR(50),
+   last_name VARCHAR(50),
    
- //   phone VARCHAR(15),
- //    city VARCHAR(30),
- //     looking_for VARCHAR(100),
- //     email VARCHAR(30),
- //     password VARCHAR(50)
+   phone VARCHAR(15),
+    city VARCHAR(30),
+     looking_for VARCHAR(100),
+     email VARCHAR(30),
+     password VARCHAR(50)
 
- //   )";
+   )";
 
-  // $create_table_result =  $herokupostgrsdatabse->query($create_table_query);
+  $create_table_result =  $herokupostgrsdatabse->query($create_table_query);
 
 
    $insert_query = "insert into registered_users (first_name,last_name,phone,city,looking_for,email,password) 
@@ -41,10 +37,4 @@ $herokupostgrsdatabse = new HerokuPostgresDatabase();
 ?>
 
 
-<!--  [salutation] => Mr.
-    [first_name] => avinash pawar
-    [last_name] => ,
-    [email] => avinash@gmail.com
-    [phone] => 8793087125
-    [city] => Pune
-    [00N90000002GeUl] => One Person Company Registration -->
+
